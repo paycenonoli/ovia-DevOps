3eac4b61c9f72e1ca32319fae5c8323d07dbf628
chief-priest <sylviapascale@yahoo.com>
2026-03-08 23:38:33 -0500

    First commit for DevOps course

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Hello, Welcome to OVIA-THINKNXY DEVOPS!"
+
+
+@app.route("/services")
+def services():
+    return "Version 1 work in progress!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
